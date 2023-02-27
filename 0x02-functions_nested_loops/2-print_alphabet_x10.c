7461dc7ea856643ebd7e1ee5a48b625c9c510ea4
Boris Eyambe <bcem693@gmail.com>
2023-02-27 01:27:31 -0800

    alphabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints lowercase alphabet 10 times
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char c;
+	int i;
+
+	for (i =0; i < 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
+
