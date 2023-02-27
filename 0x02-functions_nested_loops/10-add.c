4061edf1dc9c98fc1cc9829bf77c537c56132422
Boris Eyambe <bcem693@gmail.com>
2023-02-27 04:33:12 -0800

    adds

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * int add(int, int): a finction that add two int and return the result
+ * @a : first parameter to be add
+ * @b: second parameter to be add
+ */
+
+
+int add(int a, int b)
+{
+	return (a+b);
+}
