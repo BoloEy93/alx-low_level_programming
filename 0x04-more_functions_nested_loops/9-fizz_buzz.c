02fed14707c5c38b3b83836f96347b7643232a3d
Boris Eyambe <bcem693@gmail.com>
2023-03-06 10:50:47 -0800

    10th

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
  * main - main function
  *
  *
  * Return: a number
  */
+
 int main(void)
 {
 	int i;
@@ -21,7 +23,7 @@ int main(void)
 			{
 				printf("Buzz");
 			}
-		} else if (i % 3 ==0 && i % 5 == 0)
+		} else if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz");
 		} else 
